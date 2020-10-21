38be392c82f8b496c26251fc92ce4bb77f8a339b
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2020-10-22 01:27:27 +0900

    feat: add more test case

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -45,4 +45,44 @@ describe('renderSitemap', () => {
       expect(renderSitemap(entries)).toBe(renderedXML)
     })
   })
+
+  describe('multiple urlsets', () => {
+    it('3 entries', () => {
+      const entries: Entry[] = [
+        {
+          url: 'https://your-site/your-page-1',
+          lastmod: '2020-10-10',
+          changefreq: 'always',
+          priority: 0.8,
+        },
+        {
+          url: 'https://your-site/your-page-2',
+          lastmod: '2020-10-10',
+        },
+        {
+          url: 'https://your-site/your-page-3',
+        },
+      ]
+
+      const renderedXML =
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<urlset>' +
+        '<url>' +
+        '<loc>https://your-site/your-page-1</loc>' +
+        '<lastmod>2020-10-10</lastmod>' +
+        '<changefreq>always</changefreq>' +
+        '<priority>0.8</priority>' +
+        '</url>' +
+        '<url>' +
+        '<loc>https://your-site/your-page-2</loc>' +
+        '<lastmod>2020-10-10</lastmod>' +
+        '</url>' +
+        '<url>' +
+        '<loc>https://your-site/your-page-3</loc>' +
+        '</url>' +
+        '</urlset>'
+
+      expect(renderSitemap(entries)).toBe(renderedXML)
+    })
+  })
 })
